03f3a6a7a250fa518b972a3bc0a9fc54a4bc0e8a
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-06-04 03:16:12 -0300

    a

--- build.sh
+++ build.sh
@@ -303,7 +303,7 @@ for triplet in "${targets[@]}"; do
 	
 	cd "$(mktemp --directory)"
 	
-	declare sysroot_url="https://github.com/AmanoTeam/android-sysroot/releases/latest/download/${triplet}.tar.xz"
+	declare sysroot_url="https://github.com/AmanoTeam/android-sysroot/releases/latest/download/${triplet/-unknown//}.tar.xz"
 	declare sysroot_file="${PWD}/${triplet}.tar.xz"
 	declare sysroot_directory="${PWD}/${triplet}"
 	
