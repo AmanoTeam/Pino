74d4afefa58b4b7183e1e39d29644e2269599d6f
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-06-07 16:08:25 -0300

    Update dependencies

--- build.sh
+++ build.sh
@@ -211,6 +211,8 @@ if ! [ -f "${gcc_tarball}" ]; then
 		patch --directory="${gcc_directory}" --strip='1' --input="${name}"
 	done
 	
+	sed -i 's/throw ()/noexcept/g' /tmp/pino-toolchain/lib/gcc/*/15/include/mm_malloc.h
+	
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/0001-a.patch"
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/0001-Fix-declarations.patch"
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Fix-libgcc-build-on-arm.patch"
