56d77bcbdaf2b12d177c289c9d9274036bcb418e
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-07-24 04:09:57 -0300

    Avoid including langinfo.h on older Android

--- build.sh
+++ build.sh
@@ -44,14 +44,14 @@ declare -r optflags='-w -O2'
 declare -r linkflags='-Xlinker -s'
 
 declare -ra targets=(
-	# 'mips64el-unknown-linux-android'
-	# 'aarch64-unknown-linux-android'
-	# 'riscv64-unknown-linux-android'
-	# 'x86_64-unknown-linux-android'
+	'mips64el-unknown-linux-android'
+	'aarch64-unknown-linux-android'
+	'riscv64-unknown-linux-android'
+	'x86_64-unknown-linux-android'
 	'i686-unknown-linux-android'
-	# 'armv5-unknown-linux-androideabi'
-	# 'armv7-unknown-linux-androideabi'
-	# 'mipsel-unknown-linux-android'
+	'armv5-unknown-linux-androideabi'
+	'armv7-unknown-linux-androideabi'
+	'mipsel-unknown-linux-android'
 )
 
 declare -ra versions=(
@@ -503,9 +503,9 @@ cp "${workdir}/submodules/obggcc/tools/ln.sh" '/tmp/ln'
 export PATH="/tmp:${PATH}"
 
 # The gold linker build incorrectly detects ffsll() as unsupported.
-# if [[ "${CROSS_COMPILE_TRIPLET}" == *'-android'* ]]; then
-# export ac_cv_func_ffsll=yes
-# fi
+if [[ "${CROSS_COMPILE_TRIPLET}" == *'-android'* ]]; then
+	export ac_cv_func_ffsll=yes
+fi
 
 for triplet in "${targets[@]}"; do
 	declare extra_configure_flags=''
