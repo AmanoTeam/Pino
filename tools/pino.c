806206a39eb189ce4d419c5e2650db825f283909
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-07-24 01:18:53 -0300

    Fix declaration of localeconv()

--- tools/pino.c
+++ tools/pino.c
@@ -7,13 +7,7 @@
 
 void* mmap64(void* __addr, size_t __size, int __prot, int __flags, int __fd, off64_t __offset) {
 	
-	int __mmap2_shift = 12; /* 2**12 == 4096 */
-	
-	const int page_size = getpagesize();
-	
-	if (page_size == 16384) {
-		__mmap2_shift += 2; /* 2**14 == 16384 */
-	}
+	const int __mmap2_shift = 12; /* 2**12 == 4096 */
 	
 	if (__offset < 0 || (__offset & ((1UL << __mmap2_shift) - 1)) != 0) {
 		errno = EINVAL;
@@ -21,7 +15,7 @@ void* mmap64(void* __addr, size_t __size, int __prot, int __flags, int __fd, off
 	}
 
 	// prevent allocations large enough for `end - start` to overflow
-	size_t __rounded = __BIONIC_ALIGN(__size, page_size);
+	size_t __rounded = __BIONIC_ALIGN(__size, PAGE_SIZE);
 	
 	if (__rounded < __size || __rounded > PTRDIFF_MAX) {
 		errno = ENOMEM;
