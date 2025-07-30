86c81eee28a68e6b843b2eaa4b2b85fe9eb80852
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-07-29 22:08:37 -0300

    I have no clue what I'm doing

--- build.sh
+++ build.sh
@@ -47,14 +47,14 @@ declare -r optflags='-w -O2'
 declare -r linkflags='-Xlinker -s'
 
 declare -ra targets=(
-	'mips64el-unknown-linux-android'
-	'mipsel-unknown-linux-android'
+	# 'mips64el-unknown-linux-android'
+	# 'mipsel-unknown-linux-android'
 	'aarch64-unknown-linux-android'
-	'riscv64-unknown-linux-android'
-	'x86_64-unknown-linux-android'
-	'i686-unknown-linux-android'
-	'armv5-unknown-linux-androideabi'
-	'armv7-unknown-linux-androideabi'
+	# 'riscv64-unknown-linux-android'
+	# 'x86_64-unknown-linux-android'
+	# 'i686-unknown-linux-android'
+	# 'armv5-unknown-linux-androideabi'
+	# 'armv7-unknown-linux-androideabi'
 )
 
 declare -ra versions=(
@@ -751,8 +751,8 @@ for triplet in "${targets[@]}"; do
 	fi
 	
 	env ${args} make \
-		CFLAGS_FOR_TARGET="-fuse-ld=${linker} -D __ANDROID_API__=${base_version} ${optflags} ${linkflags}" \
-		CXXFLAGS_FOR_TARGET="-fuse-ld=${linker} -D __ANDROID_API__=${base_version} ${optflags} ${linkflags}" \
+		CFLAGS_FOR_TARGET="-fuse-ld=${linker} -D __ANDROID_MIN_SDK_VERSION__=${base_version} -D __ANDROID_API__=${base_version} ${optflags} ${linkflags}" \
+		CXXFLAGS_FOR_TARGET="-fuse-ld=${linker} -D __ANDROID_MIN_SDK_VERSION__=${base_version} -D __ANDROID_API__=${base_version} ${optflags} ${linkflags}" \
 		gcc_cv_objdump="${CROSS_COMPILE_TRIPLET}-objdump" \
 		all --jobs="${max_jobs}"
 	make install
