1170a2ed79b9936a5cdb1f78378b8bd859eb552b
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-08-02 03:01:41 -0300

    Android NDK r29 Beta 3

--- build.sh
+++ build.sh
@@ -47,14 +47,14 @@ declare -r optflags='-w -O2'
 declare -r linkflags='-Xlinker -s'
 
 declare -ra targets=(
-	# 'riscv64-unknown-linux-android'
-	# 'x86_64-unknown-linux-android'
-	# 'i686-unknown-linux-android'
-	# 'armv5-unknown-linux-androideabi'
+	'riscv64-unknown-linux-android'
+	'x86_64-unknown-linux-android'
+	'i686-unknown-linux-android'
+	'armv5-unknown-linux-androideabi'
 	'armv7-unknown-linux-androideabi'
-	# 'mips64el-unknown-linux-android'
-	# 'mipsel-unknown-linux-android'
-	# 'aarch64-unknown-linux-android'
+	'mips64el-unknown-linux-android'
+	'mipsel-unknown-linux-android'
+	'aarch64-unknown-linux-android'
 )
 
 declare -ra versions=(
