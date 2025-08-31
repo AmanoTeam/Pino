65e5379d92a88a5b6fc8b116b903840e34426928
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-08-31 01:21:27 -0300

    Handle -D__ANDROID_UNAVAILABLE_SYMBOLS_ARE_WEAK__

--- build.sh
+++ build.sh
@@ -41,6 +41,8 @@ declare -r zstd_directory='/tmp/zstd-dev'
 declare -r nz_tarball='/tmp/nz.tar.xz'
 declare -r nz_directory='/tmp/nouzen'
 
+declare -r include_unified_directory="${toolchain_directory}/include/android-unified"
+
 declare nz='1'
 
 declare -r max_jobs='30'
@@ -346,7 +348,6 @@ if ! [ -f "${gcc_tarball}" ]; then
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/0001-Ignore-pragma-weak-when-the-declaration-is-private-o.patch"
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/0001-AArch64-enable-libquadmath.patch"
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/0001-configure-Add-enable-autolink-libatomic-use-in-LINK_.patch"
-	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/0001-On-linux-targets-pass-as-needed-by-default-to-the-li.patch"
 	
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Fix-libgcc-build-on-arm.patch"
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Change-the-default-language-version-for-C-compilatio.patch"
@@ -892,6 +893,21 @@ for triplet in "${targets[@]}"; do
 		
 		rm --recursive "${libsanitizer_directory}"
 	fi
+	
+	if ! [ -d "${include_unified_directory}" ]; then
+		cp --recursive "${toolchain_directory}/${triplet}/include" "${include_unified_directory}"
+		rm --force --recursive "${include_unified_directory}/asm"*
+	fi
+	
+	for name in "${toolchain_directory}/${triplet}/include/"*; do
+		if [[ "${name}" == *'/asm'* ]]; then
+			continue
+		fi
+		
+		rm --force --recursive "${name}"
+	done
+	
+	ln --symbolic --relative "${include_unified_directory}/"* "${toolchain_directory}/${triplet}/include"
 	
 	for version in "${versions[@]}"; do
 		declare sysroot_url="https://github.com/AmanoTeam/android-sysroot/releases/latest/download/${target}${version}.tar.xz"
