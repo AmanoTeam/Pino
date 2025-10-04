0f99f5e5be1fa2477dca015d58010d454757a577
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-10-04 14:15:37 -0300

    Revert "configure: Always add pre-installed header directories to search path"

--- build.sh
+++ build.sh
@@ -26,10 +26,10 @@ declare -r isl_directory='/tmp/isl-0.27'
 declare -r binutils_tarball='/tmp/binutils.tar.xz'
 declare -r binutils_directory='/tmp/binutils'
 
-declare -r gcc_major='15'
+declare -r gcc_major='16'
 
 declare -r gcc_tarball='/tmp/gcc.tar.xz'
-declare -r gcc_directory="/tmp/gcc-releases-gcc-${gcc_major}"
+declare -r gcc_directory="/tmp/gcc-master"
 
 declare -r libsanitizer_tarball='/tmp/libsanitizer.tar.xz'
 declare -r libsanitizer_directory='/tmp/libsanitizer'
