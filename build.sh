0e79ea9a20d928eae66e6d33041e06601ea20169
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-04-29 03:26:40 -0300

    a

--- build.sh
+++ build.sh
@@ -573,7 +573,6 @@ if ! [ -f "${gcc_tarball}" ]; then
 	
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Prevent-libstdc-from-trying-to-implement-math-stubs.patch"
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/0001-Enable-automatic-linking-of-libandroid.patch"
-	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/0001-Revert-libstdc-Export-explicit-instantiations-of-__format-__do_vformat_to.patch"
 fi
 
 # Follow Debian's approach to remove hardcoded RPATHs from binaries
@@ -1112,7 +1111,7 @@ for triplet in "${targets[@]}"; do
 		CXXFLAGS_FOR_TARGET="${target_cxxflags}" \
 		LDFLAGS_FOR_TARGET="${linkflags}" \
 		gcc_cv_objdump="${host}-objdump" \
-		all --jobs="${max_jobs}"
+		all --jobs=1 # "${max_jobs}"
 	env ${args} make install
 	
 	make -C "${workdir}/tools/stubs" clean
